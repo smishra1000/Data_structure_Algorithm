79cb5387af3c9f52ce68ab87f56ec7d27c0c5980
smishra1000 <smishra1000@rediffmail.com>
2024-01-18 20:22:34 +0530

    11_meidan_of_two_sorted_array

---
+++ DSA-DAILY/11_median_of_two_array.js
@@ -0,0 +1,48 @@
+
+function getMedian(ar1, ar2, n)
+{
+    var i = 0; 
+    var j = 0; 
+    var count;
+    var m1 = -1, m2 = -1;
+
+    for (count = 0; count <= n; count++)
+    {
+        if (i == n)
+        {
+            m1 = m2;
+            m2 = ar2[0];
+            break;
+        }
+
+        else if (j == n)
+        {
+            m1 = m2;
+            m2 = ar1[0];
+            break;
+        }
+        if (ar1[i] <= ar2[j])
+        {
+            m1 = m2; 
+            m2 = ar1[i];
+            i++;
+        }
+        else
+        {
+            m1 = m2; 
+            m2 = ar2[j];
+            j++;
+        }
+    }
+ 
+    return (m1 + m2)/2;
+}
+
+let ar1 = [1, 12, 15, 26, 38];
+let ar2 = [2, 13, 17, 30, 45];
+let n1 = ar1.length;
+let n2 = ar2.length;
+if (n1 == n2)
+    console.log("Median is "+ getMedian(ar1, ar2, n1));
+else
+    console.log("Doesn't work for arrays of unequal size");
